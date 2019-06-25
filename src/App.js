b4f524cb1a276811e773ab89ca6232170b02d808
Christopher Tso <christso@gmail.com>
2019-06-25 17:02:07 +1000

    Fix None shelf

--- src/App.js
+++ src/App.js
@@ -14,6 +14,11 @@ class BooksApp extends React.Component {
     };
   }
 
+  camelToTitle = (camelCase) => camelCase
+    .replace(/([A-Z])/g, (match) => ` ${match}`)
+    .replace(/^./, (match) => match.toUpperCase())
+    .trim()
+
   componentDidMount() {
     BooksAPI.getAll().then(books => {
       const booksByShelf = books.reduce((pv, x) => {
@@ -31,26 +36,24 @@ class BooksApp extends React.Component {
       return booksByShelf[value].find(b => b.id === book.id);
     });
     const inShelf = inShelfs.length === 0 ? null : inShelfs[0];
+
     this.setState((currState) => ({
       ...currState,
       booksByShelf: {
         ...currState.booksByShelf,
-        [shelf]: [...currState.booksByShelf[shelf], book],
+        [shelf]: (currState.booksByShelf.hasOwnProperty(shelf)
+          ? [...currState.booksByShelf[shelf], book] : [book]),
         [inShelf]: currState.booksByShelf[inShelf].filter(x => x.id !== book.id)
       }
-    }));  
+    }));
 
     // todo: call Api
   }
 
   render() {
+    console.log('render');
     console.log('state', this.state.booksByShelf);
 
-    const camelToTitle = (camelCase) => camelCase
-    .replace(/([A-Z])/g, (match) => ` ${match}`)
-    .replace(/^./, (match) => match.toUpperCase())
-    .trim()
-    
     return (
       <div>
         <Route exact path='/' render={() =>
@@ -58,12 +61,14 @@ class BooksApp extends React.Component {
             <div className="list-books-title">
               <h1>MyReads</h1>
             </div>
-            {Object.keys(this.state.booksByShelf).map(shelf => 
-              <ListBooks key={shelf} books={this.state.booksByShelf[shelf]} 
+            {Object.keys(this.state.booksByShelf).map(shelf =>
+              this.state.booksByShelf[shelf].length &&
+              <ListBooks key={shelf} books={this.state.booksByShelf[shelf]}
                 shelf={shelf}
-                heading={camelToTitle(shelf)}
+                heading={this.camelToTitle(shelf)}
                 onChangeShelf={this.handleChangeShelf}
-              />)}
+              />
+            )}
           </div>} />
         <Route path='/search' component={SearchBooks} />
       </div>
