1a0f977f8684aedd31ac104f0a3aa2cacb700cf8
Christopher Tso <christso@gmail.com>
2019-06-25 10:05:34 +1000

    get books by shelf

--- src/test.js
+++ src/test.js
@@ -1,2 +1,16 @@
-var result = Math.random().toString(36).substr(-8);
-console.log(result);
+const api = "https://reactnd-books-api.udacity.com"
+
+let token = '3yflt3a8';
+
+const headers = {
+  'Accept': 'application/json',
+  'Authorization': token
+}
+
+const getAll = () =>
+  fetch(`${api}/books`, { headers })
+    .then(res => res.json())
+    .then(data => data.books);
+
+fetch(`${api}/books`, { headers })
+  .then(res => console.log(res));
